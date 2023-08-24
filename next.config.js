e50d143afe1e17727865ca5a9bcc9e97931139f2
Joel Shaw <jshaw64@users.noreply.github.com>
2023-08-23 21:24:04 -0700

    Add minimal i18n config to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
   async rewrites() {
     return {
       beforeFiles: [
